e777d8152e5d81d1ba2d3eb128162122b276f6e3
stephdim <stephane.dimiceli@gmail.com>
2016-03-20 17:47:44 +0100

    Show Info about existing scripts on a gameObject
    Display in UI the list of renderer properties

---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.EventSystems;
+
+public class ColorPicker : MonoBehaviour {
+    
+    [SerializeField]
+    Texture2D texture2D;
+
+    Renderer parentRenderer;
+
+	// Use this for initialization
+	void Start () {
+	    gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseEnter() {
+        parentRenderer = transform.parent.GetComponent<Renderer>();
+    }
+
+    void OnMouseOver() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            parentRenderer.material.color = texture2D.GetPixel(Mathf.FloorToInt(hit.textureCoord.x * texture2D.width), Mathf.FloorToInt(hit.textureCoord.y * texture2D.height));
+        }
+    }
+
+    void OnMouseDown() {
+        transform.SetParent(null);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/DisplayComponents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DisplayComponents : MonoBehaviour {
+
+    GameObject colorPicker;
+    Text text;
+	// Use this for initialization
+	void Awake () {
+	    colorPicker = GameObject.Find("ColorPicker");
+	    text = GameObject.Find("Text").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnMouseDown() {
+        foreach (var component in GetComponents(typeof(Component))) {
+            var rend = component as Renderer;
+            Type t = component.GetType();
+            if (rend != null) {
+
+                foreach (var field in t.GetProperties()) {
+                    text.text += field.Name + "\n";
+
+                    if (field.Name.Equals("material")) {
+                        field.PropertyType.GetProperty("color").SetValue(rend.material,Color.white, null);
+                    }
+                }
+                colorPicker.transform.SetParent(gameObject.transform);
+                colorPicker.SetActive(true);
+            }
+        }
+    }
+}
